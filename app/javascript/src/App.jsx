d0ddb16e84bc04cc00c6f35309f2c76fe43a2a9b
aaronsajo <aaronalpt@gmail.com>
2021-11-04 01:36:33 +0530

    Added login page

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import { setAuthHeaders } from "apis/axios";
+import Login from "components/Authentication/Login";
 
 import { initializeLogger } from "./common/logger";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
-
+  // const authToken = getFromLocalStorage("authToken");
+  // const isLoggedIn = !either(isNil, isEmpty)(authToken) && authToken !== "null";
   useEffect(() => {
     initializeLogger();
     setAuthHeaders(setLoading);
@@ -23,6 +25,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" render={() => <div>Home ALL</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </Router>
   );
