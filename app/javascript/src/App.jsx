bd15757156d9922bd2a8ad4f5217eb4e7e7cd139
aaronsajo <aaronalpt@gmail.com>
2021-11-03 23:07:51 +0530

    Added API based architecture

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/" render={() => <div>Home ALL</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
       </Switch>
     </Router>
