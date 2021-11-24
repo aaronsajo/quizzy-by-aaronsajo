85b5be166a96215f652355c482c9799c62a7b0f2
aaronsajo <aaronalpt@gmail.com>
2021-11-24 14:13:03 +0530

    Added 422 response if slug is invalid

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -58,8 +58,13 @@ class QuizzesController < ApplicationController
 
   def check_slug
     quiz_slug = Quiz.find_by(slug: params[:slug])
-    @id = quiz_slug ? quiz_slug.id : nil
-    @title = quiz_slug ? quiz_slug.title : nil
+    if quiz_slug
+      @id = quiz_slug ? quiz_slug.id : nil
+      @title = quiz_slug ? quiz_slug.title : nil
+    else
+      render status: :unprocessable_entity,
+        json: { error: t("not_found", entity: "Quiz") }
+    end
   end
 
   def report
